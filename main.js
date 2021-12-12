931cbb99019042768986e1a1e1406bb2f60eaf25
Jeffer Marcelino <jeffersunde72@gmail.com>
2021-12-12 16:31:39 +0200

    Adding scrollreveal

--- main.js
+++ main.js
@@ -44,3 +44,21 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
 });
+
+/* ScrollReaveal */
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duaration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, 
+    {interval: 100}
+)
