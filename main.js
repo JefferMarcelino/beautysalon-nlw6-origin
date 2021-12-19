30f0d10fb4ae5facd289a7747d14eec2887020b5
Jeffer Marcelino <jeffersunde72@gmail.com>
2021-12-19 03:47:04 +0200

    active link

--- main.js
+++ main.js
@@ -21,9 +21,10 @@ for(const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll () {
-    const header = document.querySelector("#header")
-    const navHeight = header.offsetHeight
+    
     if(window.scrollY >= navHeight) {
         // scrool é maior que a altura do header
         header.classList.add("scroll")
@@ -72,8 +73,8 @@ scrollReveal.reveal(
 )
 
 /* Botão voltar para o topo */
+const backToTopButton = document.querySelector(".back-to-top")
 function backToTop () {
-    const backToTopButton = document.querySelector(".back-to-top")
 
     if(window.scrollY >= 560) {
         backToTopButton.classList.add("show")
@@ -82,9 +83,37 @@ function backToTop () {
     }
 }
 
+/* Menu ativo conforme a secção visível na página */
+const sections = document.querySelectorAll("main section[id]")
+function activateMenuAtCurrentSection () {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute("id")
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                    .querySelector("nav ul li a[href*=" + sectionId + "]")
+                    .classList.add("active")
+
+        } else {
+            document
+                .querySelector("nav ul li a[href*=" + sectionId + "]")
+                .classList.remove("active")
+        }
+    }
+}
+
 
 /* When Scroll */
 window.addEventListener("scroll", function (){
     backToTop()
     changeHeaderWhenScroll()
+    activateMenuAtCurrentSection ()
 })
+
